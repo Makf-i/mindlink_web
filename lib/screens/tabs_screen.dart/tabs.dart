e14e767a06cd121deebf2f96e9573f72b86692a5
Makfi-i <althafkf1915@gmail.com>
2024-09-27 18:25:42 +0530

    sec

--- lib/screens/tabs_screen.dart/tabs.dart
+++ lib/screens/tabs_screen.dart/tabs.dart
@@ -159,7 +159,7 @@ class NavigationRailWidget extends StatelessWidget {
       destinations: const [
         NavigationRailDestination(
           icon: Icon(Icons.text_fields),
-          label: Text('Text'),
+          label: Text('Texts'),
         ),
         NavigationRailDestination(
           icon: Icon(Icons.video_library),
